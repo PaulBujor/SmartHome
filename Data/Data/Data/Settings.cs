9f7a03e08ba35a6dbaabb41f3d66ae47c083ae4e
Paul <bujor.paul2000@gmail.com>
2021-04-18 12:44:51 +0200

    Update Settings.cs

--- Data/Data/Data/Settings.cs
+++ Data/Data/Data/Settings.cs
@@ -17,6 +17,18 @@ namespace Data.Data
 		public CO2Configuration CO2Configuration { get; set; }
 		public HumidityConfiguration HumidityConfiguration { get; set; }
 
+		public Settings Defaults()
+		{
+			return new Settings
+			{
+				DeviceConfiguration = new DeviceConfiguration { Active = true },
+				AlarmConfiguration = new AlarmConfiguration { Active = true, MinOrDefault = 0.5 },
+				CO2Configuration = new CO2Configuration { Active = true, Max = 650, MinOrDefault = 200 },
+				HumidityConfiguration = new HumidityConfiguration { Active = true, Max = 50, MinOrDefault = 30 },
+				TemperatureConfiguration = new TemperatureConfiguration { Active = true, Max = 23, MinOrDefault = 20 }
+			};
+		}
+
 
 		/* Default settings
 		 * Settings
