c437bc682b24aafa06b70e727cde419ee54f4281
AlexandruTatarciuc <tatarciucalexandru@gmail.com>
2021-04-18 13:37:23 +0200

    fix destinations for navigation drawer

--- Android/app/src/main/java/com/example/myhomeapplication/View/MainActivity.java
+++ Android/app/src/main/java/com/example/myhomeapplication/View/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myhomeapplication.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private NavController navController;
     private DrawerLayout drawerLayout;
     private AppBarConfiguration appBarConfiguration;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +32,20 @@ public class MainActivity extends AppCompatActivity {
         navController = navHostFragment.getNavController();
 
         drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.nav_view);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        /*setSupportActionBar(toolbar);*/
+
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).setOpenableLayout(drawerLayout).build();
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-/*        setSupportActionBar(toolbar);*/
-
         NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+        //Allows navigation to destinations when clicking menu items in the navigation drawer
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         return navController.navigateUp() || super.onSupportNavigateUp();
     }
+
 }
