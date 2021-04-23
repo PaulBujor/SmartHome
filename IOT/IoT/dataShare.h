ea682e0812fe98785dc575aef5daab40f0a11e89
Dobre-Mircea <dobremirceaionut@gmail.com>
2021-04-23 17:25:24 +0200

    Lora Connected and sending messages in hex properly

---
+++ IOT/IoT/dataShare.h
@@ -0,0 +1,26 @@
+/*
+ * dataShare.h
+ *
+ * Created: 23-Apr-21 3:14:33 PM
+ *  Author: dobre
+ */ 
+
+
+#ifndef DATASHARE_H_
+#define DATASHARE_H_
+
+extern float temperature;
+extern float humidity;
+
+extern uint16_t ppm;
+
+extern uint16_t lastSoundValue;
+
+
+extern bool motion;
+
+
+
+
+
+#endif /* DATASHARE_H_ */
