da9e1d128fef8ff2a8a6e50e883418902bd95ac0
Paul <bujor.paul2000@gmail.com>
2021-04-26 12:57:28 +0200

    removed console logging

--- Data/APIBridge/src/main/java/WebsocketClient.java
+++ Data/APIBridge/src/main/java/WebsocketClient.java
@@ -1,6 +1,9 @@
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.WebSocket;
@@ -11,6 +14,7 @@ import java.util.concurrent.CompletionStage;
 public class WebsocketClient implements WebSocket.Listener {
     private WebSocket server = null;
     private Bridge bridge;
+    private PrintWriter logger;
 
     // Send down-link message to device
     // Must be in Json format according to https://github.com/ihavn/IoT_Semester_project/blob/master/LORA_NETWORK_SERVER.md
@@ -19,6 +23,17 @@ public class WebsocketClient implements WebSocket.Listener {
     }
 
     public WebsocketClient(String url, Bridge bridge) {
+        File file = new File("logs.txt");
+        try {
+            logger = new PrintWriter(file);
+            new Thread(() -> {
+                while(true) {
+                    Thread.sleep(5*);
+                }
+            }).start();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         this.bridge = bridge;
         HttpClient client = HttpClient.newHttpClient();
         CompletableFuture<WebSocket> ws = client.newWebSocketBuilder()
@@ -30,39 +45,40 @@ public class WebsocketClient implements WebSocket.Listener {
     public void onOpen(WebSocket webSocket) {
         // This WebSocket will invoke onText, onBinary, onPing, onPong or onClose methods on the associated listener (i.e. receive methods) up to n more times
         webSocket.request(1);
-        System.out.println("WebSocket Listener has been opened for requests.");
+        logger.println("WebSocket Listener has been opened for requests.");
     }
 
     public void onError​(WebSocket webSocket, Throwable error) {
-        System.out.println("A " + error.getCause() + " exception was thrown.");
-        System.out.println("Message: " + error.getLocalizedMessage());
+        logger.println("A " + error.getCause() + " exception was thrown.");
+        logger.println("Message: " + error.getLocalizedMessage());
         webSocket.abort();
+        logger.close();
     }
 
     ;
 
     public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
-        System.out.println("WebSocket closed!");
-        System.out.println("Status:" + statusCode + " Reason: " + reason);
-        return new CompletableFuture().completedFuture("onClose() completed.").thenAccept(System.out::println);
+        logger.println("WebSocket closed!");
+        logger.println("Status:" + statusCode + " Reason: " + reason);
+        return new CompletableFuture().completedFuture("onClose() completed.").thenAccept(logger::println);
     }
 
     ;
 
     public CompletionStage<?> onPing​(WebSocket webSocket, ByteBuffer message) {
         webSocket.request(1);
-        System.out.println("Ping: Client ---> Server");
-        System.out.println(message.asCharBuffer().toString());
-        return new CompletableFuture().completedFuture("Ping completed.").thenAccept(System.out::println);
+        logger.println("Ping: Client ---> Server");
+        logger.println(message.asCharBuffer().toString());
+        return new CompletableFuture().completedFuture("Ping completed.").thenAccept(logger::println);
     }
 
     ;
 
     public CompletionStage<?> onPong​(WebSocket webSocket, ByteBuffer message) {
         webSocket.request(1);
-        System.out.println("Pong: Client ---> Server");
-        System.out.println(message.asCharBuffer().toString());
-        return new CompletableFuture().completedFuture("Pong completed.").thenAccept(System.out::println);
+        logger.println("Pong: Client ---> Server");
+        logger.println(message.asCharBuffer().toString());
+        return new CompletableFuture().completedFuture("Pong completed.").thenAccept(logger::println);
     }
 
     ;
@@ -88,10 +104,10 @@ public class WebsocketClient implements WebSocket.Listener {
 
         if (cmd.equalsIgnoreCase("rx")) {
             bridge.addData(Long.parseLong(deviceId, 16), hexData);
-            return new CompletableFuture().completedFuture("onText() completed.").thenAccept(System.out::println);
+            return new CompletableFuture().completedFuture("onText() completed.").thenAccept(logger::println);
         }
 
-        return new CompletableFuture().completedFuture("onText() not complete due to wrong cmd identifier.").thenAccept(System.out::println);
+        return new CompletableFuture().completedFuture("onText() not complete due to wrong cmd identifier.").thenAccept(logger::println);
     }
 
     ;
