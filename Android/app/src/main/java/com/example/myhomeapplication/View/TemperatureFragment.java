f6e43cfaac4e9805b71cfc5add9c7c69e6cc173c
AlexandruTatarciuc <tatarciucalexandru@gmail.com>
2021-05-20 18:02:27 +0200

    graph updates

--- Android/app/src/main/java/com/example/myhomeapplication/View/TemperatureFragment.java
+++ Android/app/src/main/java/com/example/myhomeapplication/View/TemperatureFragment.java
@@ -1,16 +1,11 @@
 package com.example.myhomeapplication.View;
 
-import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,8 +13,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import com.example.myhomeapplication.Custom.XAxisValueFormatter;
 import com.example.myhomeapplication.Local_Persistence.MeasurementTypes;
 import com.example.myhomeapplication.Models.Measurement;
 import com.example.myhomeapplication.Models.TemperatureRecyclerAdapter;
@@ -86,6 +81,9 @@ public class TemperatureFragment extends Fragment {
         temperatureGraph.setDrawBorders(false);
         temperatureGraph.setDescription(null);
         temperatureGraph.getLegend().setEnabled(false);
+        //To fix labels going off the screen
+        temperatureGraph.setExtraLeftOffset(10);
+        temperatureGraph.setExtraRightOffset(30);
 
         //XAxis
         XAxis xAxis = temperatureGraph.getXAxis();
@@ -93,6 +91,8 @@ public class TemperatureFragment extends Fragment {
         xAxis.setDrawGridLines(false);
         xAxis.setDrawAxisLine(false);
         xAxis.setValueFormatter(new XAxisValueFormatter());
+        xAxis.setLabelCount(5, true);
+        //xAxis.setXOffset(20f);
 
         //Right YAxis - disabled
         YAxis yr = temperatureGraph.getAxisRight();
@@ -102,7 +102,7 @@ public class TemperatureFragment extends Fragment {
         //Left YAxis
         YAxis yl = temperatureGraph.getAxisLeft();
         yl.setSpaceTop(30);
-        yl.setSpaceBottom(20);
+        //yl.setSpaceBottom(20);
 
         recyclerView = view.findViewById(R.id.recyclerViewTemperatureDetails);
         recyclerView.hasFixedSize();
@@ -140,6 +140,7 @@ public class TemperatureFragment extends Fragment {
         set1.setHighlightLineWidth(2f);
         set1.setDrawHorizontalHighlightIndicator(false);
         set1.setDrawValues(true);
+        set1.setMode(LineDataSet.Mode.CUBIC_BEZIER);
 
         return new LineData(set1);
     }
