d90fc103b939f220d5b9a4ac086df42d170ed69b
AlexandruTatarciuc <tatarciucalexandru@gmail.com>
2021-05-24 14:50:58 +0200

    device settings update

--- Android/app/src/main/java/com/example/myhomeapplication/View/DeviceSettingsFragment.java
+++ Android/app/src/main/java/com/example/myhomeapplication/View/DeviceSettingsFragment.java
@@ -18,11 +18,6 @@ import com.example.myhomeapplication.R;
 
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DeviceSettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class DeviceSettingsFragment extends Fragment implements DeviceAdapter.OnDeviceItemClickedListener, View.OnClickListener {
 
     private View view;
@@ -31,44 +26,14 @@ public class DeviceSettingsFragment extends Fragment implements DeviceAdapter.On
     private EditText tempMin, tempMax, humMin, humMax, cO2min, cO2max, soundMin, soundMax;
     private Button addDevice, removeDevice;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public DeviceSettingsFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DeviceSettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DeviceSettingsFragment newInstance(String param1, String param2) {
-        DeviceSettingsFragment fragment = new DeviceSettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
